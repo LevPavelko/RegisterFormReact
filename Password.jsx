ee506db8b0c8e2710671cca8f1961d6b6c2e1fd0
LevPavelko <lev.pavelko.studies@gmail.com>
2024-05-22 13:25:19 +0200

    Task3: Добавление валидации

--- Password.jsx
+++ Password.jsx
@@ -1,12 +1,15 @@
 class Password extends React.Component{
+   
     constructor(props){
         super(props);
         let isValid = this.validate(props.value);
         this.state = { value: props.value, valid: isValid };
         this.onChange = this.onChange.bind(this);
+
+        
     }
     validate(val) {
-        return val !== ''; 
+        return val !== '' && val.length >= 3 && val.length <= 10;
     }
     onChange(e) {
         let val = e.target.value;
@@ -14,7 +17,14 @@ class Password extends React.Component{
         this.setState({ value: val, valid: isValid });
     }
     render() {
-        let validation = this.state.valid === true ? "" : "Заполните поле";
+        let validation = "";
+        if (this.state.value === '') {
+            validation = "Заполните поле";
+        } else if (!this.state.valid) {
+            validation = "Пароль должен быть от 3 до 10 символов";
+        }
+       
+      
         return (
             <div>
                 <label>Password:</label>
